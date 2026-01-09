52bd241711bd3bb6a0f1d16549af858be935e56e
MuellerPh <philipp.mueller2@haufe-lexware.com>
2026-01-09 10:44:20 +0100

    ADD COLUMN ON TABLE THREADS AND MESSAGES
    Add column deleted_at and updated_at, together with an index on `deleted_at` for threads and messages.

--- db/migrations/000001_THREADS_MESSAGES_create_table.down.sql
+++ db/migrations/000001_THREADS_MESSAGES_create_table.down.sql
@@ -2,10 +2,12 @@ BEGIN;
 
 DROP INDEX IF EXISTS idx_messages_id;
 DROP INDEX IF EXISTS idx_messages_created_at;
+DROP INDEX IF EXISTS idx_messages_deleted_at;
 DROP TABLE IF EXISTS messages;
 
 DROP INDEX IF EXISTS idx_threads_id;
 DROP INDEX IF EXISTS idx_threads_created_at;
+DROP INDEX IF EXISTS idx_threads_deleted_at;
 DROP TABLE IF EXISTS threads;
 
 COMMIT;
--- db/migrations/000001_THREADS_MESSAGES_create_table.up.sql
+++ db/migrations/000001_THREADS_MESSAGES_create_table.up.sql
@@ -7,10 +7,13 @@ CREATE TABLE IF NOT EXISTS threads (
     pk bigint generated always as identity PRIMARY KEY,
     id uuid UNIQUE NOT NULL DEFAULT gen_random_uuid(),
     title TEXT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NULL,
+    deleted_at TIMESTAMPTZ NULL
 );
 CREATE INDEX IF NOT EXISTS idx_threads_id ON threads(id);
 CREATE INDEX IF NOT EXISTS idx_threads_created_at ON threads(created_at);
+CREATE INDEX IF NOT EXISTS idx_threads_deleted_at ON threads(deleted_at);
 
 -- Create the MESSAGES table
 CREATE TABLE IF NOT EXISTS messages (
@@ -18,9 +21,12 @@ CREATE TABLE IF NOT EXISTS messages (
     id uuid UNIQUE NOT NULL DEFAULT gen_random_uuid(),
     thread_id uuid REFERENCES threads(id),
     content TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NULL,
+    deleted_at TIMESTAMPTZ NULL
 );
 CREATE INDEX IF NOT EXISTS idx_messages_id ON messages(id);
 CREATE INDEX IF NOT EXISTS idx_messages_created_at ON messages(created_at);
+CREATE INDEX IF NOT EXISTS idx_messages_deleted_at ON messages(deleted_at);
 
 COMMIT;
