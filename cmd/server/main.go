c58260381dfdde8a5fe5787d3f61d6d5250a07c6
MuellerPh <philipp.mueller2@haufe-lexware.com>
2026-01-09 15:47:02 +0100

    REFACTOR remove debug line

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
 
 	"examples.com/assistants/db/repository"
 	"examples.com/assistants/internal/config"
 	"examples.com/assistants/internal/db"
 	"examples.com/assistants/internal/server"
 	"examples.com/assistants/internal/services"
-	"github.com/google/uuid"
 )
 
 func main() {
@@ -30,9 +28,6 @@ func main() {
 	queries := repository.New(conn)
 	messageService := services.NewMessageService(ctx, *queries)
 	threadService := services.NewThreadService(ctx, *queries)
-
-	messages := messageService.GetMessagesByThreadID(uuid.Must(uuid.Parse("69359037-9599-48e7-b8f2-48393c019135")))
-	fmt.Printf("Messages: %+v\n", messages)
 
 	s := server.NewServer()
 	s.SetupRoutes(messageService, threadService)
